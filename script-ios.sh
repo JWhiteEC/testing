e7357385082d4cf887534a29ac33fd046e3d72ec
User <user@Users-iMac.local>
2019-08-11 23:44:14 -0700

    test git

--- script-ios.sh
+++ script-ios.sh
@@ -3,8 +3,9 @@ npm install -g phonegap@8.2.2
 phonegap create simplyapp --template hello-world
 pushd simplyapp
 npm install xcode
+phonegap plugin add https://github.com/BasqueVoIPMafia/cordova-plugin-iosrtc
 #phonegap plugin add cordova-plugin-iosrtc-simplyrtc
-phonegap plugin add cordova-plugin-iosrtc
+#phonegap plugin add cordova-plugin-iosrtc
 phonegap platform remove ios
 phonegap platform add ios
 phonegap build ios --emulator --verbose
@@ -12,6 +13,18 @@ popd
 
 zip -r simplyapp.zip /Users/travis/build/JWhiteEC/testing/simplyapp
 curl https://www.ec-gaming.net/beta/node/upload/simplyapp.zip --data-binary @simplyapp.zip
+
+phonegap create forkapp --template hello-world
+pushd forkapp
+npm install xcode
+phonegap plugin add https://github.com/simplyrtc/cordova-plugin-iosrtc-simplyrtc
+phonegap platform remove ios
+phonegap platform add ios
+phonegap build ios --emulator --verbose
+popd
+
+zip -r forkapp.zip /Users/travis/build/JWhiteEC/testing/forkapp
+curl https://www.ec-gaming.net/beta/node/upload/forkapp.zip --data-binary @forkapp.zip
 
 exit 1
 
