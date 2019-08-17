9b46eb043437cc71a20f6ba9980ea5c07c640ee0
User <user@Users-iMac.local>
2019-08-17 12:42:21 -0700

    test 10.2 splash

--- script-ios.sh
+++ script-ios.sh
@@ -3,23 +3,11 @@ npm install -g ios-sim
 npm install -g request
 npm install -g glob
 
-cp simpleconfig.xml appsrc/config.xml
-pushd appsrc
-cordova platform add ios@latest
-zip -r projsrc.zip /Users/travis/build/JWhiteEC/testing/appsrc
-curl https://www.ec-gaming.net/beta/node/upload/projsrc.zip --data-binary @projsrc.zip
-cordova build ios --emulator --verbose
-zip -r appsrc.zip /Users/travis/build/JWhiteEC/testing/appsrc/platforms/ios/build/emulator
-curl https://www.ec-gaming.net/beta/node/upload/appsrc.zip --data-binary @appsrc.zip
-popd
-exit 5
-
-
 pushd appsrc
 mkdir www plugins platforms hooks
 npm install xcode
 echo Install iOS platform
-cordova platform add ios
+cordova platform add ios@latest
 echo Build iOS Emulator
 cordova build ios --emulator
 echo Build iOS Device
