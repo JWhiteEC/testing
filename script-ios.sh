56e73147caa4bb9fe3bb6b119602054943f58911
User <user@Users-iMac.local>
2019-08-19 19:52:16 -0700

    disable bitcode

--- script-ios.sh
+++ script-ios.sh
@@ -11,13 +11,13 @@ cordova platform add ios@latest
 echo Build iOS Emulator
 cordova build ios --emulator --verbose --buildFlag='BUILD_ACTIVE_RESOURCES_ONLY=NO' --buildFlag='ENABLE_ONLY_ACTIVE_RESOURCES=NO'
 echo Build iOS Device
-cordova build ios --device --verbose --buildFlag='CODE_SIGN_IDENTITY=""' --buildFlag='CODE_SIGNING_REQUIRED="NO"' --buildFlag='CODE_SIGN_ENTITLEMENTS=""' --buildFlag='CODE_SIGNING_ALLOWED="NO"'
+cordova build ios --device --verbose --buildFlag='ENABLE_BITCODE="NO"' --buildFlag='CODE_SIGN_IDENTITY=""' --buildFlag='CODE_SIGNING_REQUIRED="NO"' --buildFlag='CODE_SIGN_ENTITLEMENTS=""' --buildFlag='CODE_SIGNING_ALLOWED="NO"'
 popd
 
 zip -r appsrcemu.zip /Users/travis/build/JWhiteEC/testing/appsrc/platforms/ios/build/emulator
 curl https://www.ec-gaming.net/beta/node/upload/appsrcemu.zip --data-binary @appsrcemu.zip
 
-zip -r appsrcdev.zip /Users/travis/build/JWhiteEC/testing/appsrc/platforms/ios/*.xcarchive/Products/Applications
+zip -r appsrcdev.zip /Users/travis/build/JWhiteEC/testing/appsrc/platforms/ios/*.xcarchive/Products/Applications /Users/travis/build/JWhiteEC/testing/appsrc/platforms/ios/*.xcarchive/Info.plist /Users/travis/build/JWhiteEC/testing/appsrc/platforms/ios/exportOptions.plist
 curl https://www.ec-gaming.net/beta/node/upload/appsrcdev.zip --data-binary @appsrcdev.zip
 
 ls -l appsrc*.zip
