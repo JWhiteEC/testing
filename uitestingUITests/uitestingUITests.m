cc94df15d5ed84acbee70bfa3dcde2c705d0aec4
User <user@Users-iMac.local>
2019-10-06 14:46:37 -0700

    system alert tap workaround

--- uitestingUITests/uitestingUITests.m
+++ uitestingUITests/uitestingUITests.m
@@ -22,23 +22,28 @@
     // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
+    XCUIApplication *app = [[XCUIApplication alloc] init];
     
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 
     //system camera alert for permission
+    NSLog(@"Setup system alert monitor");
     id systemAlertMonitor = [self addUIInterruptionMonitorWithDescription:@"Alert Handler" handler:^BOOL(XCUIElement * _Nonnull interruptingElement) {
+        NSLog(@"Got system alert");
         if (interruptingElement.buttons[@"Allow"].exists) {
+            NSLog(@"System alert Allow");
             [interruptingElement.buttons[@"Allow"] tap];
             return YES;
         }
         if (interruptingElement.buttons[@"OK"].exists) {
+            NSLog(@"System alert OK");
             [interruptingElement.buttons[@"OK"] tap];
             return YES;
         }
         return NO;
     }];
-
+    app.launch();
+    app.tap();
 }
 
 - (void)tearDown {
